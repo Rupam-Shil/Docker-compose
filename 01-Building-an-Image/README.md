b8cc4df30fbd96c55c019ede11917e65bf4e690f
Rupam Shil <rupamshil111@gmail.com>
2021-12-12 20:01:51 +0530

    new files added

--- 01-Building-an-Image/README.md
+++ 01-Building-an-Image/README.md
@@ -16,3 +16,28 @@ docker images
 docker image rm express-app:v2
 # to remove the image
 ```
+
+- running container
+
+```
+docker run -d --name express-container express-app
+```
+
+- to check running container
+
+```
+docker ps
+```
+
+- to remove totally
+
+```
+docker remove express-app
+```
+
+- to stop the container
+
+```
+docker stop express-container   #do clean away and they stop the container
+docker kill express-container   # do right away
+```
--- 01-Building-an-Image/index.js
+++ 01-Building-an-Image/index.js
@@ -1,14 +1,13 @@
 const axios = require('axios');
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', async (req, res) => {
-    let image = await axios('https://source.unsplash.com/random');
-    res.send(`<img src="${image.request.res.responseUrl}" />`)
-})
+	let image = await axios('https://source.unsplash.com/random');
+	res.send(`<img src="${image.request.res.responseUrl}" />`);
+});
 
-
-app.listen((3333), () => {
-    console.log("listening on port 3333!!!!!")
-})
+app.listen(3333, () => {
+	console.log('listening on port 3333!!!!!!!');
+});
